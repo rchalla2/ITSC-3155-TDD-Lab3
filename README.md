c1e303fce3d3f2ef7ac1ad1a15c79b4c9b3e5489
rchalla2 <rchalla2@uncc.edu>
2022-04-10 23:11:53 -0400

    Completed lab assignment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ITSC-3155-TDD-Lab3-
+# ITSC-3155-TDD-Lab3
+
+ITSC 3155 Test Driven Dev Lab 3
---
+++ TestInvoice.py
@@ -0,0 +1,24 @@
+
+import pytest
+from Invoice import Invoice
+
+@pytest.fixture
+def products():
+	products = {
+		"Pen": {"qnt": 10, "unit_price": 3.75, "discount": 5},
+		"Notebook": {"qnt": 5, "unit_price": 7.5, "discount": 10}
+	}
+	return products
+
+@pytest.fixture
+def invoice():
+	return Invoice()
+
+def test_CanCalculateTotalImpurePrice(invoice, products):
+	assert invoice.totalImpurePrice(products) == 75
+
+def test_CanCalculateTotalDiscount(invoice, products):
+	assert invoice.totalDiscount(products) == 5.62
+
+def test_CanCalculateTotalPurePrice(invoice, products):
+	assert invoice.totalPurePrice(products) == 69.38
